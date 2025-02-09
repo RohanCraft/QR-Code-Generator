f298c1535dea9c2ec81ae08dfa5ecde4c92613dd
Rohan kumar sahoo <rohankumar9969@gmail.com>
2025-02-09 14:58:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let imgBox = document.querySelector(".imgBox");
+let qrImg = document.querySelector(".qrImg");
+let qrText = document.querySelector(".qrText");
+let generatebtn = document.querySelector(".generatebtn");
+
+
+function generateQR(){
+    if(qrText.value != ''){
+
+        qrImg.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value ;
+
+    imgBox.classList.add("show-img");
+
+    }
+    else{
+        qrText.classList.add("error");
+        setTimeout(()=>{
+        qrText.classList.remove("error");
+        },1000);
+    }
+    
+}
+
+generatebtn.addEventListener("click",generateQR);
+document.addEventListener("keydown",(event)=>{
+    if(event.key === "Enter"){
+        generateQR();
+    }
+})
